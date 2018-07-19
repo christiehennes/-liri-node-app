b9bdd07754918234dd30c65c981d74c85786b7ed
Christie Hennes <christiehennes@gmail.com>
2018-07-18 17:22:42 -0700

    Updated twitter formatting

--- liri.js
+++ liri.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const keys = require('./keys.js');
 const $ = (require('jquery'));
 
-debug = 0;
+debug = 1;
 
 
 //Initalize Twitter
@@ -21,27 +21,39 @@ var request = require("request");
 let userInput = process.argv.slice(2);
 if (debug){console.log(userInput);} 
 
-
-
-//Process the user input with a switch statement 
-
+//Set variables for the command line arguments
 let command = userInput[0];
 let arg = userInput[1];
 
+//Process the user input with a switch statement 
 switch (command){
 
     case 'my-tweets':
-    if(debug) console.log("in my tweets");
 
-    client.get('statuses/user_timeline', {screen_name: 'christiehennes'}, function(error, tweets, response) {
-        if(error) console.log(error);
-        console.log(tweets);  // The favorites.
-        console.log(response);  // Raw response object.
-      });
+        if(debug) console.log("in my tweets");
+
+        //Make the Twitter call and display tweet information
+        client.get('statuses/user_timeline', {screen_name: 'christiehennes'}, function(error, tweets, response) {
+            if(error) console.log(error);
+            
+            tweets.forEach(tweet => {
+                console.log(`
+                ${tweet.text}
+                ${tweet.created_at}
+                
+                `)
+            });
+        });
+
         break;
 
     case 'spotify-this-song':
         if(debug) console.log("in spotify this song");
+
+        //Search for The Sign if the usere did not enter a song
+        if (arg === undefined){
+            arg = '"The Sign" by Ace of Base';
+        }
 
         spotify.search({ type: 'track', query: arg }, function(err, data) {
             if (err) {
@@ -67,30 +79,37 @@ switch (command){
 
 
         });
+
         break;
 
     case 'movie-this':
 
-    request('http://www.omdbapi.com/?t=' + arg + '&y=&plot=short&apikey=d7c89213', function(error, response, body) {
-
-        // If the request is successful (i.e. if the response status code is 200)
-        if (!error && response.statusCode === 200) {
-      
-          // Parse the body of the site and recover just the imdbRating
-          // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
-          console.log(`
-          Title: ${JSON.parse(body).Title}
-          Year: ${JSON.parse(body).Year}
-          IMDB Rating: ${JSON.parse(body).Ratings[0].Value}
-          Rotten Tomatoes Rating: ${JSON.parse(body).Ratings[1].Value}
-          Country: ${JSON.parse(body).Country}
-          Language: ${JSON.parse(body).Language}
-          Plot: ${JSON.parse(body).Plot}
-          Actors: ${JSON.parse(body).Actors}
-          `);
-
+        //Set the argument to Mr. Nobody if the user did not enter a movie 
+        if(arg === undefined){
+            arg = 'Mr. Nobody';
         }
-      });
+            
+        //Make the OMDB request via request 
+        request('http://www.omdbapi.com/?t=' + arg + '&y=&plot=short&apikey=d7c89213', function(error, response, body) {
+
+            // If the request is successful (i.e. if the response status code is 200)
+            if (!error && response.statusCode === 200) {
+        
+            // Parse the body of the response and display movie info
+            console.log(`
+            Title: ${JSON.parse(body).Title}
+            Year: ${JSON.parse(body).Year}
+            IMDB Rating: ${JSON.parse(body).Ratings[0].Value}
+            Rotten Tomatoes Rating: ${JSON.parse(body).Ratings[1].Value}
+            Country: ${JSON.parse(body).Country}
+            Language: ${JSON.parse(body).Language}
+            Plot: ${JSON.parse(body).Plot}
+            Actors: ${JSON.parse(body).Actors}
+            `);
+
+            }
+        });
+        
         break;
 
     case 'do-what-it-says':
